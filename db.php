06f9ebfd1f5b0f9a433f544451346cd121689610
nan <nan@gmail.com>
2025-07-28 08:00:01 +0800

    update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 require_once "SupabasePDO.php"; // Custom class you'll need to implement
 
-$apiUrl = "https://fwyorzhxakxbdawkvmta.supabase.co";
-$apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3eW9yemh4YWt4YmRhd2t2bXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MjY2ODUsImV4cCI6MjA2OTEwMjY4NX0.uThuifm7Wbf5ogff-jVj52SWVmydp5dQTHimDk_L354";
+$apiUrl = "https://hcrtnwdvfcrjftahzybm.supabase.co";
+$apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcnRud2R2ZmNyamZ0YWh6eWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NTM1NzEsImV4cCI6MjA2NzQyOTU3MX0.SKAL-qnyTFGiwqO9-7-pFeuzw9TQXcYtfM49gB6Ae8I";
 
 try {
   $pdo = new SupabasePDO($apiUrl, $apiKey); // You must define this class
